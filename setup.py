373d745f6d462c86ab4d35c702437200a57247db
Gregory Clark <gkclarkie@gmail.com>
2020-05-10 22:55:28 -0500

    created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='sshaman',
+      version='1.0',
+      description='a simple ssh manager',
+      author='gianator',
+      author_email='gianator@gianator.me',
+      url='https://github.com/gianator/sshaman'
+)
