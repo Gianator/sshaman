742970f063747c99add9b38beb1e9c3755aee204
Gianator <gianator@gianator.me>
2020-06-05 12:26:18 -0500

    created connection class

--- sshaman/connection.py
+++ sshaman/connection.py
@@ -1,3 +1,34 @@
+from os import system
+
 class Connection(object):
-    def __init__(self):
-        print(1)
+    def __init__(self, alias: str, connection: str, env: str = None):
+        self.alias = alias
+        self.connection = connection
+        self.env = env
+
+        self.password = None
+
+    def connect(self):
+        if self.password is None:
+            self.password = self.decrypt()
+        
+        system(self.env + ' sshpass -p ' + self.password + ' ssh ' + self.connection)
+       
+    def edit(self):
+        pass
+
+    def decrypt(self):
+        pass
+
+    def __str__(self):
+        string = self.alias + ":\n"
+        string += "  " + self.connection + "\n"
+        if self.env is not None:
+            string += "  " + self.env
+        return string
+
+
+if __name__ == '__main__':
+    con = Connection("droplet", "greg@ory.land", "TERM=xterm")
+    print(con)
+    con.connect()
